8ae0362660d55e996de9a430fb3f7787e94a3231
나평근 <skvudrms54@gmail.com>
2024-10-07 14:29:27 +0900

    feat: 실패한 이벤트 처리

--- consumer/src/main/java/com/example/consumer/repository/CouponRepository.java
+++ consumer/src/main/java/com/example/consumer/repository/CouponRepository.java
@@ -1,4 +1,4 @@
-package com.example.consumer.consumer;
+package com.example.consumer.repository;
 
 import com.example.consumer.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
