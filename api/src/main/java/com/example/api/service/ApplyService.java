8d419716f12a17ceec687758654d4d833618af06
나평근 <skvudrms54@gmail.com>
2024-10-07 11:21:42 +0900

    feat: solve Race condition with redis INCR command

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +9,23 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
 
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     // RaceCondition
 
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        // Reids는 싱글 스레드기반으로 작동하기 때문에 이전 작업이 끝날때 까지 진행 x
+        // 항상 최신 값으로 처리하게 됨 -> 하지만 이 구조에서도 발생하는 문제가 있다.
+        // 발생하는 문제
+        // 발급하는 쿠폰이 많아지면 많아진다면 DB에 부하가 발생
+        // 쿠폰 전용 DB가 아니라면 에러가 전파됨
+        long count = couponCountRepository.increment();
 
         if (count > 100) {
             return;
